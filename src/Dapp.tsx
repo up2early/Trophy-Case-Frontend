7d356f0cd574fbaffb11eb4f13a728ddf2ff0325
up2early <wakeup2early@gmail.com>
2022-03-11 02:00:33 -0700

    Rewrote Dapp.tsx to be a functional component

--- src/Dapp.tsx
+++ src/Dapp.tsx
@@ -1,142 +1,118 @@
-import React, { Component, useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { TransactionView } from "./view/TransactionView"
 import { WalletContainer } from "./container/WalletContainer"
 import { web3getGreeting, web3getSigner, web3updateGreeting } from "./data/Web3Data"
 import { GreetingContainer } from "./container/GreetingContainer"
 import { Provider, Web3Provider } from "zksync-web3"
 
-interface IDappProps {
-}
+export const Dapp = () => {
+    const [wallet, setWallet] = useState<{ address: string, connected: boolean, provider: Web3Provider | null }>({ address: "", connected: false, provider: null })
+    const [web3dataProvider, setWeb3datProvider] = useState(new Provider("https://zksync2-testnet.zksync.dev"))
+    const [notificationData, setNotificationData] = useState({ message: "", dismissed: true })
+    const [greeting, setGreeting] = useState("")
 
-interface IDappState {
-    address: string
-    walletConnected: boolean
-    txBeingSent: boolean
-    txError: string
-    networkError: boolean
-    txHash: string
-    txDismissed: boolean
-    provider: any
-    greeting: string
-}
-
-export class Dapp extends Component<IDappProps, IDappState> {
-    initialState: IDappState
-    constructor(props: IDappProps) {
-        super(props)
-
-        const address = ""
-        const walletConnected = false
-        const txBeingSent = false
-        const txError = ""
-        const networkError = false
-        const txHash = ""
-        const txDismissed = false
-        const provider = new Provider("https://zksync2-testnet.zksync.dev")
-        const greeting = "Loading..."
-
-        this.initialState = { address, walletConnected, txBeingSent, txError, networkError, txHash, txDismissed, provider, greeting }
-
-        this.state = this.initialState
-    }
-
-    render() {
-        return (
-            <>
-                {this.state.txHash !== "" &&
-                    <this.TransactionComponent />
-                }
-
-                <this.WalletComponent />
-
-                {this.state.walletConnected &&
-                    <this.GreetingComponent />
-                }
-            </>
-        )
-    }
-
-    TransactionComponent = () => {
+    const TransactionComponent = () => {
         const dismissTx = () => {
-            this.setState({ txDismissed: true })
+            setNotificationData({ message: "", dismissed: true })
         }
 
         return (
             <TransactionView
-                txHash={this.state.txHash}
-                txBeingSent={this.state.txBeingSent}
-                txError={this.state.txError}
-                dismissed={this.state.txDismissed}
+                txHash={" tx Hash "}
+                txBeingSent={true}
+                txError={" tx Error "}
+                dismissed={notificationData.dismissed}
                 onDismiss={dismissTx}
             />
         )
     }
 
-    WalletComponent = () => {
+    const WalletComponent = () => {
+        const resetWalletState = () => {
+            setWallet({ address: "", connected: false, provider: null })
+        }
+
         const connectWallet = async () => {
-            if (this.state.walletConnected) {
-                this.setState(this.initialState)
+            if (wallet?.connected) {
+                resetWalletState()
             } else {
-                const resetState = () => {
-                    this.setState(this.initialState)
+                const updateDappWalletState = (address: string, provider: Web3Provider) => {
+                    setWallet({ address, connected: true, provider })
                 }
 
-                const updateDappWalletState = (selectedAddress: string, provider: Web3Provider) => {
-                    this.setState({ walletConnected: true, provider , address: selectedAddress })
-                }
-
-                await web3getSigner(resetState, updateDappWalletState)
+                await web3getSigner(resetWalletState, updateDappWalletState)
             }
         }
 
         return (
             <WalletContainer
-                address={this.state.address}
-                connected={this.state.walletConnected}
+                address={wallet.address}
+                connected={wallet.connected}
                 onConnect={connectWallet}
             />
         )
     }
 
-    GreetingComponent = () => {
+    const GreetingComponent = () => {
+        async function updateGreetingState() {
+            // You can await here
+            const greeting = await web3getGreeting(web3dataProvider)
+            setGreeting(greeting)
+        }
+        const setTxError = () => {
+            console.log("Not Implemented")
+            return
+        }
+
+        const setTxInProgress = () => {
+            console.log("Not Implemented")
+            return
+        }
+
+        const setTxDone = () => {
+            updateGreetingState()
+            return
+        }
+
         useEffect(() => {
-            async function fetchData(provider: Provider, setState: Function) {
-                // You can await here
-                const greeting = await web3getGreeting(provider)
-                setState(greeting)
+            updateGreetingState();
+        }, []) // Runs only once
+        
+        const provider = wallet.provider
+
+        if (provider) {
+            return (
+                <GreetingContainer
+                    setGreeting={async (greeting: string) => {
+                        await web3updateGreeting(greeting, provider, setTxInProgress, setTxError, setTxDone)
+                    }}
+                    greeting={greeting}
+                />
+            )
+        } else {
+            return (
+                <GreetingContainer
+                    setGreeting={async (greeting: string) => {
+                        setNotificationData({ message: "Please connect your wallet", dismissed: false})
+                    }}
+                    greeting={greeting}
+                />
+            )
+
+        }
+    }
+
+    return (
+        <>
+            {!notificationData.dismissed &&
+                <TransactionComponent />
             }
 
-            fetchData(
-                this.state.provider,
-                (greeting: string) => {
-                    this.setState({ greeting })
-                }
-            );
-        }, []) // <-- empty dependency array
-        return (
-            <GreetingContainer
-                setGreeting={async (greeting: string) => {
-                    await web3updateGreeting(greeting, this.state.provider, this.setTxInProgrees, this.setTxError, this.setTxDone)
-                }}
-                greeting={this.state.greeting}
-            />
-        )
-    }
+            <WalletComponent />
 
-    // TODO: Move these functions to be inside the component they're used in
-    setTxError = () => {
-        console.log("Not Implemented")
-        return
-    }
-
-    setTxInProgrees = () => {
-        console.log("Not Implemented")
-        return
-    }
-
-    setTxDone = async () => {
-        const greeting = await web3getGreeting(this.state.provider)
-        this.setState({ greeting })
-        return
-    }
+            {wallet.connected &&
+                <GreetingComponent />
+            }
+        </>
+    )
 }
